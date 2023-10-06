0cfb008f97bc8ad2c67e5675711e9f3c93910f56
SharonR11 <sharon.rudas@tecsup.edu.pe>
2023-10-05 21:47:20 -0500

    commit

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
 var _ = require("underscore");
 var param_replacer = require('./lib/replace');
-var objetivo = "%hello% %world%! -- %world% %hello%!";
 
-var idioma = "es";
-var reemplazos = {
-    "en":{
-        "hello":"Hello",
-        "world":"World"
-    },
-    "es":{
-        "hello":"Hola",
-        "world":"Mundo"
+if (typeof exports !== 'undefined'){
+    if (typeof module !== 'undefined' && module.exports){
+        exports = module.exports = param_replacer;
     }
-};
-
-var resultado = param_replacer.replace(objetivo, reemplazos[idioma]);
-
-console.log(resultado);
+    exports.param_replacer = param_replacer;
+}else {
+    root.param_replacer = param_replacer;
+}
